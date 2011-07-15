dd2d7e1019cebe7c0c817904c5ddea3cc6234051
Fernando Arconada <fernando.arconada@gmail.com>
2011-07-15 17:58:46 +0200

    limpieza

--- typoscript/footer.ts
+++ typoscript/footer.ts
@@ -2,23 +2,6 @@ lib.footer = COA
 lib.footer {
 	 9 = TEXT
 	 9.value = <div id="footerMenu">
-	10 = HMENU
-	# defines the menu entrylevel
-	10.entryLevel = 0
-	10.wrap =
-	10.1 = TMENU
-	# Wrap the first level of the menu inside an unordered list
-	10.1.wrap = <ul id="footerMenuWeb">|</ul>
-	# Formatting of menu normal entries as list items.
-	10.1.NO {
-				wrapItemAndSub = <li class="normal minus">|</li>
-	}
-	# Current menu item is unlinked and wrapped in span
-	10.1.CUR {
-				wrapItemAndSub = <li class="current minus">|</li>
-				doNotLinkIt = 0
-				allWrap = <span>|</span>
-	}
 
 	20 = HMENU
 	20 {
@@ -27,15 +10,15 @@ lib.footer {
 					1 = TMENU
 					1 {
 									noBlur = 1
-									wrap = <ul id="footerMenuUtils">|</ul>
+									wrap = <ul id="footerMenuUtils" class="ui-bar" data-role="controlgroup" data-type="horizontal" >|</ul>
 									NO = 1
 									NO.ATagTitle.field = abstract // description // subtitle
-									NO.allWrap = <li>|</li>
+									NO.allWrap = <li data-role="button">|</li>
 									NO.linkWrap = |
 									NO.stdWrap.htmlSpecialChars = 1
 									CUR < .NO
 									CUR = 1
-									CUR.allWrap = <li><strong>|</strong></li>
+									CUR.allWrap = <li data-role="button"><strong>|</strong></li>
 									CUR.doNotLinkIt = 1
 									CUR.stdWrap.htmlSpecialChars = 1
 						 }
--- typoscript/skin_typoscript.ts
+++ typoscript/skin_typoscript.ts
@@ -80,17 +80,43 @@ lib.parseFunc_RTE.nonTypoTagStdWrap.encapsLines.addAttributes.P.class >
 
 header >
 lib.myheader = COA
-lib.myheader.wrap = <div id="header" data-role="header"> <div class="interior">| </div><!-- interior end--></div>
+lib.myheader.wrap = <div id="header" data-role="header"> |<!-- interior end--></div>
 
 # Add the masthead for site title / logo.
 lib.myheader.10 = COA
 lib.myheader.10 {
-	wrap = <div id="masthead"> | </div>
 	10 = TEXT
         10 {
              data = page:title
              wrap = <h1>|</h1>
             }
+        20 = TEXT
+	20 {
+		value = Home
+		typolink.parameter = {$siteURL}
+		typolink.ATagParams = data-icon="home" data-iconpos="notext" data-direction="reverse" class="ui-btn-right jqm-home"
+		wrap = |
+	}
+	30 = HMENU
+	30 {
+					special = list
+					special.value = {$headerNavigationIncludeList}
+					1 = TMENU
+					1 {
+									noBlur = 1
+									wrap = <div data-role="navbar"><ul>|</ul></div>
+									NO = 1
+									NO.ATagTitle.field = abstract // description // subtitle
+									NO.allWrap = <li>|</li>
+									NO.linkWrap = |
+									NO.stdWrap.htmlSpecialChars = 1
+									CUR < .NO
+									CUR = 1
+									CUR.allWrap = <li class="ui-btn-active">|</li>
+									CUR.doNotLinkIt = 1
+									CUR.stdWrap.htmlSpecialChars = 1
+						 }
+	}
 
 }
 
